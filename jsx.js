b1464c24034c1cd81f582b754fb038c10a44e4fc
Vsevolod Strukchinsky <floatdrop@gmail.com>
2014-12-09 21:44:35 +0500

    Remove opts

--- jsx.js
+++ jsx.js
@@ -1,7 +1,7 @@
 var react = require('react-tools');
 
 exports.translate = function(load) {
-	return react.transform(load.source, opts);
+	return react.transform(load.source);
 };
 
 exports.instantiate = function(load) {
