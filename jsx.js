5cdcbe5a868d02dec0c4c21dfdb927e39bf69daa
Vsevolod Strukchinsky <floatdrop@gmail.com>
2014-12-09 22:04:43 +0500

    Fixing code execution

--- jsx.js
+++ jsx.js
@@ -3,7 +3,3 @@ var react = require('react-tools');
 exports.translate = function(load) {
 	load.source = react.transform(load.source);
 };
-
-exports.instantiate = function(load) {
-	return load.source;
-};
