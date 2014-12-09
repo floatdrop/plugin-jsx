9e5da62b9eb2e4f5768987ff6172881da3acfca2
Vsevolod Strukchinsky <floatdrop@gmail.com>
2014-12-09 22:00:06 +0500

    Reassign load.source

--- jsx.js
+++ jsx.js
@@ -1,7 +1,7 @@
 var react = require('react-tools');
 
 exports.translate = function(load) {
-	return react.transform(load.source);
+	load.source = react.transform(load.source);
 };
 
 exports.instantiate = function(load) {
