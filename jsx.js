29239362cc9f1cfa8ba24d553206ce86f8241001
Guy Bedford <guybedford@gmail.com>
2016-02-04 13:18:07 +0200

    fix source map generation

--- jsx.js
+++ jsx.js
@@ -3,7 +3,7 @@
 var react = require('react-tools');
 
 exports.translate = function(load) {
-	var output = react.transformWithDetails(load.source, { es6module: true });
+	var output = react.transformWithDetails(load.source, { es6module: true, sourceMap: true });
 	load.source = output.code;
 	load.metadata.sourceMap = output.sourceMap;
 };
