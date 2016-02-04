967c1c82e910f7b31c9a474c052f80bc22033f5c
Guy Bedford <guybedford@gmail.com>
2016-02-04 13:36:32 +0200

    return source instead of setting

--- jsx.js
+++ jsx.js
@@ -4,6 +4,6 @@ var react = require('react-tools');
 
 exports.translate = function(load) {
 	var output = react.transformWithDetails(load.source, { es6module: true, sourceMap: true });
-	load.source = output.code;
 	load.metadata.sourceMap = output.sourceMap;
+	return output.code;
 };
